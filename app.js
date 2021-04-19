1dc4ab96d12d70a1fa386bc3f20651e5e0b5525a
Felix_PlaYz <coolsagnik2005@gmail.com>
2021-04-19 10:32:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// This template was made by FusionTerror.
+const DiscordRPC = require('discord-rpc'); // Requiring the discord-rpc package.
+const client = new DiscordRPC.Client({ transport: 'ipc' }); // Creating a client variable with is our rpc client.
+require('dotenv').config(); // Requiring the content of our .env file.
+
+(async () => {
+    client.on('ready', async () => { // Calling the ready event.
+        await client.setActivity({ // Setting the Rich Presence Activity based on what is passed in here.
+            buttons: [{ label: "Button Title Here", url: "Link For Button Here" }],
+            details: "Description",
+            largeImageKey: "rpc_icon",
+            largeImageText: "This text is shown when hovering over the largeImageKey in Discord."
+        }).catch(err => console.log(err));
+
+        console.log("Discord Rich Presence has been enabled.");
+    });
+
+    await client.login({ clientId: process.env.applicationID }).catch(console.error); // Logging into our application.
+})();
